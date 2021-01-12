695193c516ec6b6502a970669b422242811903ba
Maite Quiles Cerdà <o_o.maite@hotmail.com>
2021-01-12 12:21:34 +0100

    lesson 6

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
+            image: './assets/images/socks_green.jpg',
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
             url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
             inventory: 10,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 1, colour: 'green'},
-                {id: 2, colour: 'blue'}
+                {id: 1, colour: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2, colour: 'blue', image: './assets/images/socks_blue.jpg'}
             ],
             sizes: [36, 38, 42]
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        decrementCart(){
+            if(this.cart > 0){
+                this.cart -= 1
+            }
+        },
+        changeImg(img){
+            this.image = img
         }
     }
 })
