784e17b12cddf8107b17259913afd8de0d052565
Maite Quiles Cerdà <o_o.maite@hotmail.com>
2021-01-12 11:07:20 +0100

    lesson 5

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
-            inventory: 0,
-            onSale: true
+            inventory: 10,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 1, colour: 'green'},
+                {id: 2, colour: 'blue'}
+            ],
+            sizes: [36, 38, 42]
         }
     }
 })
