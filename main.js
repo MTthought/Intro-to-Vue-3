6ea61572f2c843d9f99231ebe614b5ceb446150d
Maite Quiles Cerdà <o_o.maite@hotmail.com>
2021-01-12 10:49:42 +0100

    lesson 4

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3'
+            url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
+            inventory: 0,
+            onSale: true
         }
     }
 })
