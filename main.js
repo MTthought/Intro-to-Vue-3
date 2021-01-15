71897e763e52af5aafc3a807dc134d3ee5892b2a
Maite Quiles Cerdà <o_o.maite@hotmail.com>
2021-01-15 16:40:41 +0100

    lesson 9

--- main.js
+++ main.js
@@ -3,47 +3,8 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            brand: 'Vue Mastery',
-            product: 'Socks',
-            activeVariant: 0,
-            url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
-            onSale: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                {id: 1, colour: 'green', image: './assets/images/socks_green.jpg', inventory: 50},
-                {id: 2, colour: 'blue', image: './assets/images/socks_blue.jpg', inventory: 0}
-            ],
-            sizes: [36, 38, 42]
+            premium: true
         }
     },
-    methods: {
-        addToCart(){
-            this.cart += 1
-        },
-        decrementCart(){
-            if(this.cart > 0){
-                this.cart -= 1
-            }
-        },
-        updateVariant(i){
-            this.activeVariant = i
-        }
-    },
-    computed: {
-        title() {
-            return `${this.brand} ${this.product}`
-        },
-        sale(){
-            if(this.onSale){
-                return `${this.title} is on sale`
-            }
-            return ''
-        },
-        img() {
-            return this.variants[this.activeVariant].image
-        },
-        inventory(){
-            return this.variants[this.activeVariant].inventory
-        }
-    }
+    methods: {}
 })
