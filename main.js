432df4765c4a65402def86eb0f5be530b9af4378
Maite Quiles Cerdà <o_o.maite@hotmail.com>
2021-01-15 17:04:47 +0100

    lesson 10

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ const app = Vue.createApp({
     // same as, data: function()
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        addToCart(productId){
+            this.cart.push(productId)
+        },
+        decrementCart(){
+            if(this.cart.length > 0){
+                this.cart.pop()
+            }
+        }
+    }
 })
